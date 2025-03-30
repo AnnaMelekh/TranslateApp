f7432f111b9bf9b9557de83051a81a8d2cde4f5d
AnnaMelekh <annasmelekhina@gmail.com>
2025-03-30 19:23:49 +0200

    Initial Commit

---
+++ TranslateApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TranslateApp
+//
+//  Created by Anna Melekhina on 30.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
